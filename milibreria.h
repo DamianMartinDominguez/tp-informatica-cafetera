e48f1c3592ed3fe11159b77c1b19cf125bcd557a
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2021-09-15 13:30:17 -0300

    se agregan las librerias y los prototipos

--- milibreria.h
+++ milibreria.h
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void esperar(int *p, int *r);
+void calentarAgua(int *p, int *r);
+void colocandoCafe(int *p, int *r);
+void servirAgua(int *p, int *r);
+void calentarLeche(int *p, int *r);
+void servirLeche(int *p, int *r);
+void mezclarBebida(int *p, int *r);
