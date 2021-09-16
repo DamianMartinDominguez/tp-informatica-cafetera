bc11e89b21d5c01369bdc5f66e554178ad5ef042
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2021-09-16 13:53:17 -0300

    se elimino el puntero r

--- funciones.c
+++ funciones.c
@@ -1,14 +1,14 @@
 #include "milibreria.h"
 
 
- void esperar(int *p, int *r)
+ int esperar(int *p)
  {
 
  	int *pNuevo;
- 	int *rNuevo;
+ 	
 
  	pNuevo=p;
- 	rNuevo=r;
+ 
 
 
 	printf("\ningrese que desea tomar\n");
@@ -20,14 +20,14 @@
  	if((*pNuevo)!=0)
  	{
 
- 		*rNuevo=1;
+ 		return 1;
 
  	}
 
  }
 
 
- void calentarAgua(int *p, int *r)//estado 1
+ int calentarAgua(int *p)//estado 1
  {
 
  	int *pNuevo;
@@ -36,7 +36,7 @@
  	int setAgua= 85;
 
  	pNuevo=p;
- 	rNuevo=r;
+ 	
 
 
 	printf("\ningrese la temperatura del agua\n");//simboliza la lectura del sensor de temperatura
@@ -49,63 +49,62 @@
  	  printf("\nel agua esta caliente.\n");
 
       if(*pNuevo==1)//si el servicio es te pasa a servir el agua
- 		*rNuevo=3;
+ 		return 3;
  	  else//si el servicio no es te pasa a colocar cafe
- 		*rNuevo=2;
+ 		return 2;
 
     }
 
  }
 
- void colocandoCafe(int *p, int *r)//estado 2
+int colocandoCafe(int *p)//estado 2
  {
 
  	int *pNuevo;
- 	int *rNuevo;
+ 
 
 
  	pNuevo=p;
- 	rNuevo=r;
+ 
 
 
 	printf("\ncolocando cafe\n");
- 		*rNuevo=3;
+ 		return 3;
 }
 
-void servirAgua(int *p, int *r)//estado 3
+int servirAgua(int *p)//estado 3
  {
 
  	int *pNuevo;
- 	int *rNuevo;
-
+ 	
 
  	pNuevo=p;
- 	rNuevo=r;
+ 	
 
 
 	printf("\nsirviendo agua\n");
 	if(*pNuevo==1)//si el servicio es te se termina el proceso
- 		*rNuevo=0;
+ 		return 0;
  	  else
       {if(*pNuevo==2)//si el servicio es cafe solo pasa a mezclar bebida
- 		*rNuevo=6;
+ 		return 6;
  	  else//si el servicio es cafe con leche pasa a calentar leche
- 		*rNuevo=4;
+ 		return 4;
       }
 }
 
 
-void calentarLeche(int *p, int *r)//estado 4
+int calentarLeche(int *p)//estado 4
  {
 
  	int *pNuevo;
- 	int *rNuevo;
+ 
  	int temperaturaLeche=10;
  	int setLeche=75;
 
 
  	pNuevo=p;
- 	rNuevo=r;
+ 
 
     printf("\ningrese la temperatura de la leche\n");//simboliza la lectura del sensor de temperatura
 
@@ -116,37 +115,37 @@ void calentarLeche(int *p, int *r)//estado 4
     {
  	  printf("\nla leche esta caliente.\n");
 
- 		*rNuevo=5;
+ 		return 5;
     }
 }
 
-void servirLeche(int *p, int *r)//estado 5
+int servirLeche(int *p, int *r)//estado 5
  {
 
  	int *pNuevo;
- 	int *rNuevo;
+ 	
 
 
  	pNuevo=p;
- 	rNuevo=r;
+ 
 
 
 	printf("\nsirviendo leche\n");
- 		*rNuevo=6;
+ 		return 6;
 }
 
-void mezclarBebida(int *p, int *r)//estado 6
+int mezclarBebida(int *p)//estado 6
  {
 
  	int *pNuevo;
- 	int *rNuevo;
+
 
 
  	pNuevo=p;
- 	rNuevo=r;
+ 
 
 
 	printf("\nmezclando bebida\n");
- 		*rNuevo=0;
+ 		return 0;
 }
 
