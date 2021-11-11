e4eba80a8b2230354e554c0d76b5d0cc61c56884
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2021-11-11 08:14:17 -0300

    se sacaron los define

--- mainPunteroFunciones.c
+++ mainPunteroFunciones.c
@@ -1,13 +1,5 @@
 #include "milibreria.h"
 
-#define ESPERAR        0
-#define CALENTAR_AGUA  1
-#define COLOCANDO_CAFE 2
-#define SERVIR_AGUA    3
-#define CALENTAR_LECHE 4
-#define SERVIR_LECHE   5
-#define MEZCLAR_BEBIDA 6
-
 
 int main()
  {
